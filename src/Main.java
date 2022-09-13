bd393a0052b640b972f9c37dc559f04dff905bab
shwea <kashaigf@gmail.com>
2022-09-13 21:36:39 +0400

    Для проверки ДЗ Массивы 1

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,36 @@
 public class Main {
     public static void main(String[] args) {
-        task0();
-        task1()
+        task1();
+        task2();
+        task3();
     }
-    public static void task0() {
-int[] weights = {90, 91, 93, 92, 85, 87, 88, 89, 0, 0, 0, 0};
-        weights[0] = 90;
-        int januaryWeight = weights[0];
-        System.out.println(januaryWeight);
-        System.out.println(weights[0]);
-        System.out.println(weights[4]);
-        int january = 0;
-        System.out.println(weights[january]);
-        for (int i = 0; i < weights.length; i++) {
-            System.out.println(weights[i]);
+
+    public static void task1() {
+        System.out.println("\n"+ "Задание 1");
+        int[] arr1 = new int[1];
+        int[] arr2 = new int[2];
+        int[] arr3 = new int[3];
+
+        arr1[0] = 1;
+        arr2[1] = 2;
+        arr3[2] = 3;
+        System.out.println(arr1[0]);
+        System.out.println(arr2[1]);
+        System.out.println(arr3[2]);
+    }
+
+    public static void task2() {
+        System.out.println("\n"+ "Задание 2");
+        float[] arr = new float[]{1.57f, 7.654f, 9.986f,0,0,0,0,0,0,0,0,0};
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
         }
     }
-    public static void task1() {
-
+    public static void task3() {
+        System.out.println("\n"+ "Задание 3");
+        int[] volume = new int[154];
+        volume[0] = 153;
+        int volumeFirst = volume[0];
+        System.out.println(volumeFirst);
     }
 }
